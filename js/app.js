c6e87db8daea5dc0b7fc9877d82fb6de0b3cc3fa
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-13 00:09:17 +0600

    img bug solved

--- js/app.js
+++ js/app.js
@@ -52,6 +52,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
     const image = post.image;
+    console.log(post);
     const div = document.createElement( "article" );
     div.classList.add( "post" );
     div.innerHTML = `
@@ -62,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
